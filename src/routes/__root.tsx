83905d1828920d30162ad11e42f0ea47c9223b0b
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-02-16 23:34:32 +0530

    Update routes for signed in and signed out users

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,22 @@
 import Footer from "@/components/Footer";
 import { createRootRoute, Link } from "@tanstack/react-router";
 import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import HomeIn from "@/components/SignedIn/HomeIn";
+import { NavbarIn } from "@/components/SignedIn/NavbarIn";
+import HomeOut from "@/components/SignedOut/HomeOut";
+import { NavbarOut } from "@/components/SignedOut/NavbarOut";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <SignedIn>
-        <Link to="/signedin" />
+        <NavbarIn />
+        <HomeIn />
         <SignOutButton />
       </SignedIn>
       <SignedOut>
-        <Link to="/signedout" />
+        <NavbarOut />
+        <HomeOut />
       </SignedOut>
       <Footer />
     </>
