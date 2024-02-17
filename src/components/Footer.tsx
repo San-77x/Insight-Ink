87679efa973efdf4b52d2490e50e1666bbec86cb
★ W O G G E R !!! <saneenmecheri@gmail.com>
2024-02-17 08:24:01 +0530

    Update footer background color and text color.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,21 @@ import { Facebook, Instagram, Twitter, Github, Dribbble } from "lucide-react";
 
 function Footer() {
   return (
-    <div className="  py-8 border-t bg-lime-300 mt-16">
+    <div className="  py-8 border-t bg-darkk mt-16">
       <div className="text-center">
         <a
           href="/"
           className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          <h1 className="font-mono text-4xl font-extrabold text-darkk ">
+          <h1 className="font-mono text-4xl font-extrabold text-litee ">
             InsightInk
           </h1>
         </a>
-        <p className="text-sm mb-8 -mt-5 font-mono font-semibold">
+        <p className="text-sm mb-8 -mt-5 font-mono font-semibold text-litee">
           An{" "}
           <span>
             <a
-              className="text-red-700 hover:text-red-500"
+              className="text-lime-400 hover:text-lime-600"
               href="https://bento.me/san-77x"
             >
               @San.77x
@@ -29,7 +29,7 @@ function Footer() {
           <li>
             <a
               href="/"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
+              className="text-gray-400 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
             >
               <Facebook />
             </a>
@@ -37,7 +37,7 @@ function Footer() {
           <li>
             <a
               href="/"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
+              className="text-gray-400 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
             >
               <Instagram />
             </a>
@@ -45,7 +45,7 @@ function Footer() {
           <li>
             <a
               href="/"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
+              className="text-gray-400 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
             >
               <Twitter />
             </a>
@@ -53,7 +53,7 @@ function Footer() {
           <li>
             <a
               href="/"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
+              className="text-gray-400 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
             >
               <Github />
             </a>
@@ -61,13 +61,13 @@ function Footer() {
           <li>
             <a
               href="/"
-              className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
+              className="text-gray-400 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
             >
               <Dribbble />
             </a>
           </li>
         </ul>
-        <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
+        <span className="block text-sm text-center text-gray-400 hover:text-gray-200">
           © 2024 Insight-Ink. All Rights Reserved. Built with love.
         </span>
       </div>
